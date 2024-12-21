215c7a2760b2ad4118adf701027e962637d57e24
Alexandru Michis <alexmichis@icloud.com>
2024-12-21 22:23:26 +0200

    0002: Implement HomeHero component with its styles

--- src/components/features/home/HomeHero.tsx
+++ src/components/features/home/HomeHero.tsx
@@ -1,5 +1,4 @@
-import { Separator } from "@/components/ui/separator";
-import AppStoreWhite from "@/assets/svg/AppStoreWhite.svg?react";
+import { Button } from "@/components/ui/button";
 
 const homeTitle = "Dance Cloud",
   homeSubtitle = "Oops! Something went offbeat!",
@@ -8,12 +7,26 @@ const homeTitle = "Dance Cloud",
 
 const HomeHero = () => {
   return (
-    <div>
-      <h1 className="text-9xl tracking-tighter">{homeTitle}</h1>
-      <h2 className="text-4xl tracking-tighter">{homeSubtitle}</h2>
-      <h3 className="text-2xl tracking-tighter">{homeDescription}</h3>
-      <Separator />
-      <AppStoreWhite />
+    <div className="relative flex min-h-screen w-full flex-col items-center justify-center">
+      <div className="z-10 flex flex-col items-center gap-6 p-4">
+        <h1 className="inline-block bg-[linear-gradient(to_right,#FF3BFF,#ECBFBF,#5C24FF,#D94FD5)] bg-clip-text text-5xl font-semibold tracking-tighter text-transparent sm:text-6xl md:text-8xl lg:text-9xl">
+          {homeTitle}
+        </h1>
+        <h2 className="animate-[float_2s_linear_infinite] text-xl font-medium tracking-wide sm:text-2xl md:text-4xl">
+          {homeSubtitle}
+        </h2>
+        <h3 className="max-w-3xl text-center text-lg font-normal tracking-wider text-neutral-200 sm:text-xl md:text-2xl">
+          {homeDescription}
+        </h3>
+        <Button
+          variant={"outline"}
+          size={"xl"}
+          className="after:bg-background relative cursor-pointer border-0 bg-transparent font-semibold tracking-tight shadow-[0_0_15px_rgba(255,59,255,0.5)] transition-all duration-300 before:absolute before:inset-0 before:-z-10 before:rounded-lg before:bg-[linear-gradient(to_right,#FF3BFF,#ECBFBF,#5C24FF,#D94FD5)] before:p-[1px] after:absolute after:inset-[1px] after:-z-10 after:rounded-lg hover:scale-105 hover:shadow-[0_0_20px_rgba(255,59,255,0.7)] md:py-4 md:text-lg lg:py-5 lg:text-xl xl:py-7 xl:text-2xl"
+        >
+          Open app
+        </Button>
+      </div>
+      <div className="absolute bottom-0 h-[40vh] w-full bg-gradient-to-b bg-[url('@/assets/images/hero_image.png')] from-transparent to-black bg-cover bg-bottom bg-no-repeat opacity-50 [mask-image:linear-gradient(to_bottom,transparent,black)]" />
     </div>
   );
 };
