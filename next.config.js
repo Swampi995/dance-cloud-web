3e42fcc2382a3d328112240080e70271eed896bf
Swampi995 <swp.mrs@gmail.com>
2025-01-20 19:14:21 +0200

    Try to fix redirect

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          { key: "Content-Type", value: "application/json" },
+          { key: "Cache-Control", value: "no-cache" },
+        ],
+      },
+    ];
+  },
+};
