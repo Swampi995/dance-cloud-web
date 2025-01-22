84929d4b4a283fd57010a85f454563345bfadf59
Alexandru Michis <alexmichis@icloud.com>
2025-01-22 15:43:11 +0200

    0007: Update footer layout, compress images, add store links (#16)
    * 0007: refactor: update footer layout and add app store links; remove unused files and update dependencies

--- src/components/features/landing/LandingHero.tsx
+++ src/components/features/landing/LandingHero.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { JSX } from "react";
 import {
   Carousel,
@@ -9,11 +8,15 @@ import { useMediaQuery } from "@/hooks/useMediaQuery";
 import appImage1 from "@/assets/images/app_image_1.avif";
 import appImage2 from "@/assets/images/app_image_2.avif";
 import appImage3 from "@/assets/images/app_image_3.avif";
+import AppStoreBlack from "@/assets/svg/app_store_black.svg?react";
+import GooglePlay from "@/assets/svg/google_play.svg?react";
+import Autoplay from "embla-carousel-autoplay";
 
 const title = "The ultimate platform for dance schools and dancers",
   subtitle =
     "Empower dancers to stay connected, track progress, grow their skills and streamline dance school operations with powerful management tools, all in one place.",
-  buttonLink = `smpath.dance.cloud://dancecloud`;
+  appStoreLink = `https://apps.apple.com/app/dance-cloud-all-in-one-app/id6474421119`,
+  googlePlayLink = `https://play.google.com/store/apps/details?id=smpath.dance.cloud`;
 
 const images = [
   { src: appImage1, alt: "App Image 1" },
@@ -39,7 +42,14 @@ const LandingHero = (): JSX.Element => {
   );
 
   const MobileCarousel = () => (
-    <Carousel className="relative w-full flex-grow">
+    <Carousel
+      className="relative w-full flex-grow"
+      plugins={[
+        Autoplay({
+          delay: 2000,
+        }),
+      ]}
+    >
       <CarouselContent className="h-full">
         {images.map((image, index) => (
           <CarouselItem
@@ -67,22 +77,17 @@ const LandingHero = (): JSX.Element => {
         <h2 className="px-4 text-sm font-light tracking-wide text-neutral-300 drop-shadow-[0_0_25px_rgba(255,59,255,0.5)] md:w-2/3 md:text-base lg:text-lg xl:text-xl">
           {subtitle}
         </h2>
-        <Button
-          asChild
-          variant={"outline"}
-          size={"xl"}
-          className="relative cursor-pointer border-0 bg-transparent py-4 font-semibold tracking-tight shadow-[0_0_15px_rgba(255,59,255,0.5)] transition-all duration-300 before:absolute before:inset-0 before:-z-10 before:rounded-lg before:bg-[linear-gradient(to_right,#FF3BFF,#ECBFBF,#5C24FF,#D94FD5)] before:p-[1px] after:absolute after:inset-[1px] after:-z-10 after:rounded-lg after:bg-background hover:scale-105 hover:shadow-[0_0_20px_rgba(255,59,255,0.7)] md:text-lg lg:py-5 lg:text-xl xl:py-7 xl:text-2xl"
-        >
-          <a href={buttonLink}>Open App</a>
-        </Button>
+        <div className="flex items-center gap-4">
+          <a href={appStoreLink}>
+            <AppStoreBlack />
+          </a>
+          <a href={googlePlayLink}>
+            <GooglePlay />
+          </a>
+        </div>
         {isDesktop ? <DesktopImages /> : <MobileCarousel />}
       </div>
-      <div
-        className="absolute bottom-0 h-full w-full bg-[url('@/assets/images/home_image.png')] bg-cover bg-bottom bg-no-repeat"
-        style={{
-          backgroundAttachment: "fixed", // Makes the background image fixed
-        }}
-      />
+      <div className="absolute bottom-0 h-full w-full bg-[url('@/assets/images/home_image.avif')] bg-cover bg-fixed bg-bottom bg-no-repeat" />
     </div>
   );
 };
--- src/custom.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-declare module "*.svg?react" {
-  // This is important to make the SVG work as a React component
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
-}
